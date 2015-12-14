bf3a230d7fcb5f4175949bbb5d7e68c3351fbabb
Alexander <alexander_hansson@live.se>
2015-12-14 23:12:14 +0100

    solved problem 16

---
+++ 16PowerDigitSum.py
@@ -0,0 +1,10 @@
+"""
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 21000?
+"""
+num = 2**1000
+sum = 0
+while num != 0:
+    sum+=num%10
+    num/=10
+print sum
