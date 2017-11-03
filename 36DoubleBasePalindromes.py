7f2bc8f0d58338057d21e9294fb32bb430db3c61
Alexander Hansson <alexander_hansson@live.se>
2017-11-03 16:23:43 +0100

    added comments

--- 36DoubleBasePalindromes.py
+++ 36DoubleBasePalindromes.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+"""
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+"""
+
 def doublepalindrome(string):
     for loops in range(2):
         for i in range(len(string)/2):
